7cde6d45c5634c75b723cdbe2a030b6bda55c572
athulll7 <athulsajeevan01@gmail.com>
2022-06-15 00:43:18 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:ebloodbank/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+        home: SplashScreen(),
+        theme:
+            ThemeData(primaryColor: Colors.blue, primarySwatch: Colors.blue));
+  }
+}
