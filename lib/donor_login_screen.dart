fdfefff7d64659b272b2fd9038153ee3bcb9cf0f
sharonthankachan <sharonthankachan171@gmail.com>
2022-06-26 00:19:24 +0530

    Sixth commit

--- lib/donor_login_screen.dart
+++ lib/donor_login_screen.dart
@@ -1,12 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class Donor_login_screen extends StatelessWidget {
-  const Donor_login_screen({ Key? key }) : super(key: key);
+  const Donor_login_screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Donor Login"),
-    );
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          toolbarHeight: 45,
+          backgroundColor: Colors.red,
+        ),
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    'Donor Login',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IntlPhoneField(
+                      controller: TextEditingController(),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                      ],
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.phone_android),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        hintText: 'Phone Number',
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      prefixIcon: Icon(Icons.lock_outline),
+                      hintText: 'Password',
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: Size(80, 80),
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                          shape: CircleBorder())),
+                ),
+                SizedBox(
+                  height: 55,
+                ),
+                Text(
+                  "Don't have an account?",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Sign Up',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          decoration: TextDecoration.underline)),
+                ),
+              ])),
+        )),
+        resizeToAvoidBottomInset: false);
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,196 +17,295 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String dropdownvalue = 'Select District';  
- 
-  var items = [   
+  String dropdownvalue = 'Select District';
+
+  var items = [
     'Alappuzha',
     'Ernakulam',
     'Kannur',
     'Kasaragod',
-    'Kollam','Kottayam','Wayanad','Pathanamthitta','Idukki','Malappuram','Kozhiode','Trissur','Palakkad','Tiruvanathapuram'
+    'Kollam',
+    'Kottayam',
+    'Wayanad',
+    'Pathanamthitta',
+    'Idukki',
+    'Malappuram',
+    'Kozhiode',
+    'Trissur',
+    'Palakkad',
+    'Tiruvanathapuram'
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
-    drawer: Drawer(backgroundColor: Colors.red,
-      child:ListView(
-        children: [
-          Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      drawer: Drawer(
+          backgroundColor: Colors.red,
+          child: ListView(
             children: [
-              SizedBox(height: 40,),
-
-              Image( 
-                 image: AssetImage('assets/logo ebloodbank.png'),height: 180,
-               ),
-
-              SizedBox(height: 40,),
-
-                Divider(
-                color: Colors.white,
-              ),
-
-              ListTile(
-                title: Text('- Donor Login',style: TextStyle(fontSize: 28,color:Colors.white,fontWeight: FontWeight.w400),),
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Donor_login_screen()));
-                },
-              ),
-              Divider(
-                color: Colors.white,
-              ),
-              ListTile(
-                title: Text('- Blood Bank Login',style: TextStyle(fontSize: 28,color:Colors.white,fontWeight: FontWeight.w400 ),),
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Bloodbank_login_screen()));
-                },
-              ),
-              Divider(
-                color: Colors.white,
-              ),
-              ListTile(
-                title: Text('- Requests',style: TextStyle(fontSize: 28,color:Colors.white,fontWeight: FontWeight.w400 ),),
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Requests_screen()));
-                },
-              ),
-              Divider(
-                color: Colors.white,
-              ),
-              ListTile(
-                title: Text('- Raise Request',style: TextStyle(fontSize: 28,color:Colors.white,fontWeight: FontWeight.w400 ),),
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Raise_request_screen()));
-                },
-              ),
-              Divider(
-                color: Colors.white,
-              ),
-               
+              Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Image(
+                    image: AssetImage('assets/logo ebloodbank.png'),
+                    height: 180,
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  ListTile(
+                    title: Text(
+                      '- Donor Login',
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Donor_login_screen()));
+                    },
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  ListTile(
+                    title: Text(
+                      '- Blood Bank Login',
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Bloodbank_login_screen()));
+                    },
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  ListTile(
+                    title: Text(
+                      '- Requests',
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Requests_screen()));
+                    },
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  ListTile(
+                    title: Text(
+                      '- Raise Request',
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Raise_request_screen()));
+                    },
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      )),
-    appBar: AppBar(
-      backgroundColor: Colors.red ,toolbarHeight: 40,
-    ),
-      body: SafeArea(child:
-      Column(
-        children: [
-           const SizedBox(
-            height: 30,
-           ),
-           const Image(
-                 
-                 image: AssetImage('assets/blood_drop.png'),height: 250,
-               ),
-              const SizedBox(
-            height: 20,
-           ),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: const [Text('E BLOOD BANK',
-                 style: TextStyle(
-              color: Colors.black,fontSize: 38,fontWeight: FontWeight.w900
-             ),
-                     ),
-                     ],           
-           ),
-           const SizedBox(
-            height: 50
-           ),
-           Container(width: MediaQuery.of(context).size.width*.89,
-             child: DropdownButtonHideUnderline(
-               child: DropdownButtonFormField(
-                 decoration:InputDecoration(
-                   border: OutlineInputBorder(
-                     borderRadius: BorderRadius.all(Radius.circular(15))
-                   )
-                 ),
-                 hint: Text(dropdownvalue,style: TextStyle(fontSize: 19),),
-               
-                 icon: const Icon(Icons.keyboard_arrow_down),   
-                  
-                 items: items.map((String items) {
-                   return DropdownMenuItem(
-                     value: items,
-                     child: Text(items,style: TextStyle(fontSize: 19),),
-                   );
-                 }).toList(),
-                
-                 onChanged: (String? newValue) {
-                   setState(() {
-                     dropdownvalue = newValue!;
-                   });
-                 },
-               ),
-             ),
-           ),
-          SizedBox(
-            height: 55,
-           ),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-                SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('A+',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('B+',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('AB+',style: TextStyle(fontSize: 20),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             SizedBox(              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('O+',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             
-              
-           ],),
-           SizedBox(
-            height:25 ,
-           ),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-             SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('A-',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('B-',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('AB-',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             SizedBox(
-              height: 50,width: 70,
-              child: ElevatedButton(onPressed: (){}, child: Text('O-',style: TextStyle(fontSize: 23),),style: ElevatedButton.styleFrom(primary: Colors.red))),
-             
-           ],),
-           SizedBox(
-            height: 50,
-           ),
-           Card(elevation: 15,
-           shape: const CircleBorder(),
-              color: Colors.white,
-              
-             child: Row(
+          )),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        toolbarHeight: 40,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const Image(
+              image: AssetImage('assets/blood_drop.png'),
+              height: 250,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'E BLOOD BANK',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 38,
+                      fontWeight: FontWeight.w900),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Container(
+              width: MediaQuery.of(context).size.width * .89,
+              child: DropdownButtonHideUnderline(
+                child: DropdownButtonFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)))),
+                  hint: Text(
+                    dropdownvalue,
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(
+                        items,
+                        style: TextStyle(fontSize: 19),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+                    });
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 55,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-             IconButton(onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Search_result_screen()));
-             }, icon: Icon(Icons.search),iconSize: 80,splashColor: Colors.grey,)
-             ],),
-           ),
-           
-           
-           ],
-         ) ,
-         )  
-        
-      
-    
-
-
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'A+',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'B+',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'AB+',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'O+',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'A-',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'B-',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'AB-',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+                SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'O-',
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        style: ElevatedButton.styleFrom(primary: Colors.red))),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Card(
+              elevation: 15,
+              shape: const CircleBorder(),
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Search_result_screen()));
+                    },
+                    icon: Icon(Icons.search),
+                    iconSize: 80,
+                    splashColor: Colors.grey,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
